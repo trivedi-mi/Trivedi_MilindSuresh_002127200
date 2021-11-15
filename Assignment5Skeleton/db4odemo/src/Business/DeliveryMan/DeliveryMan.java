511989b90ffb34c8390a38eb3f07ceffdd94eaa4
milin <milin@10.0.0.22>
2021-11-15 12:46:04 -0500

    Initial commit. Errors unrectified.

---
+++ Assignment5Skeleton/db4odemo/src/Business/DeliveryMan/DeliveryMan.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.DeliveryMan;
+
+/**
+ *
+ * @author harold
+ */
+public class DeliveryMan {
+    
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private int age;
+    private String driverlicense;
+    
+    public DeliveryMan(String name, String address, String email, String phoneNumber, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+//        this.driverlicense = driverlicense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDriverlicense() {
+        return driverlicense;
+    }
+
+    public void setDriverlicense(String driverlicense) {
+        this.driverlicense = driverlicense;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
